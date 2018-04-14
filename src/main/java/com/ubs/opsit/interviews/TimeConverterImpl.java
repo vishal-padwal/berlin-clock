3f8091cd9e972f2064d3ff5db38bc9f0f41546ec
Vishal <Vishal@DESKTOP-HB4C6RN>
2018-04-14 22:30:14 +0530

    Added TimeCoverterImpl

---
+++ src/main/java/com/ubs/opsit/interviews/TimeConverterImpl.java
@@ -0,0 +1,10 @@
+package com.ubs.opsit.interviews;
+
+public class TimeConverterImpl implements TimeConverter{
+
+	public String convertTime(String aTime) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
